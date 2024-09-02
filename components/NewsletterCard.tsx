f3c6616b767fe57832a36a3226a740c305a9e356
Ayush Kannaujiya <ayushkannaujiya@gmail.com>
2024-09-02 13:53:45 +0530

    nitpicking

--- components/NewsletterCard.tsx
+++ components/NewsletterCard.tsx
@@ -8,7 +8,7 @@ const NewsletterCard = () => {
             <div className="text-6xl font-sans text-orange-600 flex justify-center pt-60 pb-4">Subscribe to our Newsletter</div>
             <div className="flex justify-center pt-36 pb-60">
             <div className="flex w-full max-w-sm items-center space-x-2">
-                <Input type="email" placeholder="Email" className="border-red-600 border text-green-900 bg-green-400"/>
+                <Input type="email" placeholder="Email" className="border-green-900 border text-green-900 bg-green-400"/>
                 <Button type="submit" className="bg-green-700 hover:bg-green-900">Subscribe</Button>
             </div>
             </div>
